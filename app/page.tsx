984a9636ffed1769fb137a7f54f9bc394bb10061
marindereggi <55254483+marindereggi@users.noreply.github.com>
2023-05-13 14:41:56 +0000

    rm twitter, docs

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function IndexPage() {
       </div>
       <div className="flex gap-4">
         <Link
-          href={siteConfig.links.docs}
+          href={siteConfig.mainNav[0]}
           target="_blank"
           rel="noreferrer"
           className={buttonVariants({ size: "lg" })}
