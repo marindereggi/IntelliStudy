a57b1b9d7d93df23e57bb4979fbb4b6ee7234e8c
marindereggi <55254483+marindereggi@users.noreply.github.com>
2023-05-14 07:25:53 +0000

    Fix name

--- app/page.tsx
+++ app/page.tsx
@@ -168,9 +168,7 @@ export default function IndexPage() {
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Besedilo
-          <br className="hidden sm:inline" />
-          Besedilo
+          Pomočnik pri učenju
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
           Besedilo
