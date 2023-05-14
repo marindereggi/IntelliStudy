052ad927e80535f2855e9590c08daa1ecade9149
marindereggi <55254483+marindereggi@users.noreply.github.com>
2023-05-14 06:59:59 +0000

    Removed home

--- config/site.ts
+++ config/site.ts
@@ -3,12 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "IntelliStudy",
   description: "GPT Flashcard Generator",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
+  mainNav: [],
   links: {
     github: "https://github.com/marindereggi/IntelliStudy",
   },
